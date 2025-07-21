4950044eb58eec279a2b9f5d23caf7754d7b4226
Viswas <viswas.h@fyle.in>
2025-07-21 19:23:23 +0530

    refactor: clean up unused files

--- common/setup/integration-tests-teardown.ts
+++
@@ -1,8 +0,0 @@
-import { test as teardown } from '@playwright/test';
-import { FyleAccount } from '../services/fyle/fyle-account.service';
-
-// eslint-disable-next-line no-empty-pattern
-teardown('Integration tests teardown', async ({}) => {
-  const account = new FyleAccount(process.env.INTEGRATION_TESTS_EMAIL);
-  await account.delete(true);
-});
