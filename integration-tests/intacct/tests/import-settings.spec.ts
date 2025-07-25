9c11c7f1d064eb7ddddf61faf15b14e72361b37c
Viswas <viswas.h@fyle.in>
2025-07-25 19:52:12 +0530

    feat: test dependent fields in import settings

--- integration-tests/intacct/tests/import-settings.spec.ts
+++ integration-tests/intacct/tests/import-settings.spec.ts
@@ -58,6 +58,62 @@ test('Import settings', async ({ iframeWithIntacctSetup: iframe }) => {
     }
   });
 
+  await test.step('Dependent fields', async () => {
+    await test.step('Cost code', async () => {
+      // "Import cost code and/or cost type from Sage Intacct as dependent fields"
+      await iframe.getByRole('checkbox').check();
+
+      // Save should be disabled if neither dependent field is mapped
+      await expect(iframe.getByRole('button', { name: 'Save' })).toBeDisabled();
+
+      // Map cost code to a custom field
+      await iframe.getByRole('combobox', { name: 'Select expense field' }).first().click();
+      await iframe.getByRole('option', { name: 'custom_field' }).click();
+
+      await iframe.getByRole('textbox', { name: 'Enter field type' }).fill('E2E Cost Code');
+      await iframe.getByRole('textbox', { name: 'Enter field name' }).fill('Cost Code Placeholder');
+      await iframe.getByRole('button', { name: 'Create and save' }).click();
+
+      // Save should be enabled if at least one dependent field is mapped
+      await expect(iframe.getByRole('button', { name: 'Save' })).toBeEnabled();
+
+      // Save and assert persistence
+      await iframe.getByText('Import settings saved').waitFor({ state: 'hidden' });
+      await iframe.getByRole('button', { name: 'Save' }).click();
+      await expect(iframe.getByText('Import settings saved')).toBeVisible();
+
+      await iframe.getByRole('menuitem', { name: 'Export settings' }).click();
+      await iframe.getByRole('menuitem', { name: 'Import settings' }).click();
+
+
+      await expect(iframe.getByRole('switch', { name: 'Import cost codes' }).getByRole('switch')).toBeChecked();
+      await expect(iframe.getByRole('combobox', { name: 'E2E Cost Code' })).toBeVisible();
+    });
+
+    await test.step('Cost type', async () => {
+      // Map cost type to another custom field
+      await iframe.getByRole('combobox', { name: 'Select expense field' }).first().click();
+      await iframe.getByRole('option', { name: 'custom_field' }).click();
+
+      await iframe.getByRole('textbox', { name: 'Enter field type' }).fill('E2E Cost Type');
+      await iframe.getByRole('textbox', { name: 'Enter field name' }).fill('Cost Type Placeholder');
+      await iframe.getByRole('button', { name: 'Create and save' }).click();
+      await iframe.getByRole('switch', { name: 'Import cost type' }).click({ position: { x: 45, y: 5 } });
+
+
+      // Save and assert persistence
+      await iframe.getByText('Import settings saved').waitFor({ state: 'hidden' });
+      await iframe.getByRole('button', { name: 'Save' }).click();
+      await expect(iframe.getByText('Import settings saved')).toBeVisible();
+
+      await iframe.getByRole('menuitem', { name: 'Export settings' }).click();
+      await iframe.getByRole('menuitem', { name: 'Import settings' }).click();
+
+      await expect(iframe.getByRole('switch', { name: 'Import cost type' }).getByRole('switch')).toBeChecked();
+      await expect(iframe.getByRole('combobox', { name: 'E2E Cost Type' })).toBeVisible();
+    });
+  });
+
   await test.step('Mapping page should update', async () => {
     await iframe.getByRole('menuitem', { name: 'Mapping' }).click();
     await expect(iframe.getByRole('menuitem', { name: 'Cost center' })).toBeVisible();
